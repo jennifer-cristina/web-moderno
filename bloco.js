5dac31f4f521cdcdd601539d53a256cee44c008b
Jennifer Cristina <cristin4.jennifer@gmail.com>
2022-01-11 18:16:47 -0300

    first commit

---
+++ bloco.js
@@ -0,0 +1,9 @@
+{
+    console.log("bom dia");
+    console.log("bom tarde");
+    console.log("bom noite");
+}
+
+{
+    console.log("Passo 1"); 
+} 
---
+++ desafio_area.js
@@ -0,0 +1,8 @@
+// PI * raio * raio
+
+let raio = 10;
+const PI = 3.141592;
+let areaCirc = PI * raio * raio;
+
+console.log("O Resultado da area da Circunferencia é: " + areaCirc + "m²");
+console.log(Math.PI); //pi pelo javaScript 
